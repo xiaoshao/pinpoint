8c594a694e77e4c1ad378cacda7d38b88c42f7e0
apm <zwshao1986@gmail.com>
2018-12-10 19:51:09 +0800

    add netty demo

--- sandbox/NettyDemo/src/main/java/com/HttpHandler.java
+++ sandbox/NettyDemo/src/main/java/com/HttpHandler.java
@@ -1,4 +1,31 @@
 package com;
 
-public class HttpHandler {
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.AsciiString;
+
+public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    private AsciiString contentType = HttpHeaderValues.TEXT_PLAIN;
+
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("test".getBytes()));
+
+        HttpHeaders headers = response.headers();
+
+        headers.add(HttpHeaderNames.CONTENT_TYPE, contentType + "; charset=UTF-8");
+        headers.add(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+
+        channelHandlerContext.write(response);
+
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+        ctx.flush();
+    }
 }
--- sandbox/NettyDemo/src/main/java/com/NettyDemoMain.java
+++ sandbox/NettyDemo/src/main/java/com/NettyDemoMain.java
@@ -1,4 +1,37 @@
 package com;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
 public class NettyDemoMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ServerBootstrap b = new ServerBootstrap();
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        b.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline()
+                                .addLast("decoder", new HttpRequestDecoder())
+                                .addLast("encoder", new HttpResponseEncoder())
+                                .addLast("aggregator", new HttpObjectAggregator(512 * 1024))
+                                .addLast("handler", new HttpHandler());
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE);
+
+        b.bind(8081).sync();
+    }
 }
